f2c14eb9bc73a5788fa3b11db045b498665153b5
Eliezer de Armas <eliezerdearmas@gmail.com>
2017-09-30 12:01:22 -0400

    Add project.

---
+++ src/com/edap/sirepi/MainActivity.java
@@ -0,0 +1,15 @@
+package com.edap.sirepi;
+
+import com.edap.sirepi.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
